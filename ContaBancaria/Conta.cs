d97a5e26181f170fe02364d77b7c2588e00dea74
Leonardo <poliveira.leonardo@gmail.com>
2022-02-04 20:10:15 -0300

    Primeiro Commit

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class Conta
+    {
+
+        public int NumeroConta { get; private set; }
+        public string NomeTitularConta { get; set; }
+        public double SaldoDaConta { get; private set; }
+        
+        public Conta(int numeroConta, string nomeTitularConta)
+        {
+            NumeroConta = numeroConta;
+            NomeTitularConta = nomeTitularConta;
+            SaldoDaConta = 0;
+        }
+
+        public Conta(int numeroConta, string nomeTitularConta, double depositoInicial) : this(numeroConta, nomeTitularConta)
+        {
+            SaldoDaConta = depositoInicial;
+        }
+
+
+        public void Deposito(double valorDeposito)
+        {
+            SaldoDaConta += valorDeposito;
+            ToString();
+        }
+
+        public void Saque(double valorSaque)
+        {
+            SaldoDaConta -= valorSaque + 5.0;
+            ToString();
+        }
+
+
+        public override string ToString()
+        {
+            return "Dados da Conta:\n"
+                + $"Conta: {NumeroConta}, Titular: {NomeTitularConta}, Saldo R${SaldoDaConta}";
+        }
+
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Bem-Vindo ao banco Bamerinus!");
+            Console.WriteLine("Entre com o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o titular da conta: ");
+            string titularConta = Console.ReadLine();
+            double valorDepositoInicial = 0;
+            Console.WriteLine("Deseja fazer um deposito inicial (s/n)? ");
+            char fazerDepositoInicial = char.Parse(Console.ReadLine());
+
+            if(fazerDepositoInicial == 's' || fazerDepositoInicial == 'S')
+            {
+                Console.WriteLine("Valor para depósito: ");
+                 valorDepositoInicial = double.Parse(Console.ReadLine());
+                
+            } else
+            {
+                valorDepositoInicial = 0;
+            }
+
+            Conta usuario1 = new Conta(numeroConta, titularConta, valorDepositoInicial);
+
+            Console.WriteLine(usuario1);
+
+            Console.WriteLine("------------------------------");
+            retornoMenu:
+            Console.WriteLine("O que deseja fazer agora?\n1. Depósito\n2. Saque\n3. Ver Saldo\n9. Sair");
+
+            int escolhaMenu = int.Parse(Console.ReadLine());
+
+            switch(escolhaMenu)
+            {
+                case 1:
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Você escolheu Depósito");
+                    Console.WriteLine("Valor para depósito: ");
+                    usuario1.Deposito(double.Parse(Console.ReadLine()));
+                    Console.WriteLine(usuario1);
+                    Console.WriteLine("------------------------------");
+                    goto retornoMenu;
+                case 2:
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Você escolheu Saque");
+                    Console.WriteLine("Nosso saque tem um valor de R$5,00 por operação.");
+                    Console.WriteLine("Valor para saque: ");
+                    usuario1.Saque(double.Parse(Console.ReadLine()));
+                    Console.WriteLine(usuario1);
+                    Console.WriteLine("------------------------------");
+                    goto retornoMenu;
+                case 3:
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Você escolheu Ver Saldo");
+                    Console.WriteLine(usuario1);
+                    Console.WriteLine("------------------------------");
+                    goto retornoMenu;
+                case 9:
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Você escolheu Sair");
+                    Console.WriteLine("Até a próxima... Tchau!");
+                    Console.WriteLine("------------------------------");
+                    break;
+                default:
+                    Console.WriteLine($"{escolhaMenu} é uma opção inválida! Tente novamente");
+                    goto retornoMenu;
+            }
+        }
+    }
+}
